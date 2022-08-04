8f12be5c6749fc65369ed41e02463c526274d8cf
Frank Kobe <fkobe3@gmail.com>
2022-08-04 15:13:39 -0400

    continued with health reduction coding

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -36,6 +36,24 @@ test('gets inventory from player or returns false', () => {
 test ('gets player health value', () => {
     const player = new Player('Dave');
     
-    expect(player.getHealth).toEqual(expect.stringContaining(player.health.toString()));
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
 }); 
 
+test ('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+    expect(player.isAlive()).toBeTruthy();
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+});
+
+test ('subtract from player health', () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth (5);
+
+    expect(player.health).toBe(oldHealth - 5);
+    player.reduceHealth (99999);
+
+    expect(player.health).toBe(0);
+});
